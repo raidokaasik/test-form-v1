ac2182ea2ef5022c3fec07d23e2a7a6cf2b8170c
rkaasi <rkaasi@ttu.ee>
2023-10-16 12:00:37 +0300

    fix-path-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		host: true,
-		port: 3000,
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
 
-		// add the next lines if you're using windows and hot reload doesn't work
-		watch: {
-			usePolling: true,
-		},
-	},
-});
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      src: '/src',
+    },
+  },
+})
