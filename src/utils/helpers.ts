779fa027faaf7dc39432ca51bea0da19c0a3ea2e
rkaasi <rkaasi@ttu.ee>
2023-10-16 12:02:06 +0300

    add-helperMethod-for-register-setValueAs

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,7 @@
 export function capitalizeFirstLetter(value: string) {
   return value.charAt(0).toUpperCase() + value.slice(1)
 }
+
+export const setCaptializedValueOptions = {
+  setValueAs: (value: string) => capitalizeFirstLetter(value),
+}
