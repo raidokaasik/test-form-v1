3b481791d7f6cedefc999846a47e9920cb050323
rkaasi <rkaasi@ttu.ee>
2023-10-16 14:31:02 +0300

    add-result-page

--- src/features/form/RenderFormStage.tsx
+++ src/features/form/RenderFormStage.tsx
@@ -1,6 +1,7 @@
 import { PersonalDetailsStage } from './formStages/PersonalDetailsStage'
 import { RelationsStage } from './formStages/RelationsStage'
 import { useStateMachine } from 'little-state-machine'
+import { Result } from './formStages/Result'
 
 export const RenderFormStage = () => {
   const { state }: any = useStateMachine()
@@ -8,6 +9,7 @@ export const RenderFormStage = () => {
   const stages: any = {
     personalDetails: <PersonalDetailsStage />,
     relations: <RelationsStage />,
+    result: <Result />,
   }
 
   return <>{stages[state.formStage]}</>
---
+++ src/features/form/formStages/Result.tsx
@@ -0,0 +1,44 @@
+import { Button, Typography } from '@mui/material'
+import Box from '@mui/material/Box'
+import { useStateMachine } from 'little-state-machine'
+import { useEffect } from 'react'
+import { FormStageWrapper } from '../formWrappers/FormStageWrapper'
+import { setFormStage } from '../formActions/actions'
+
+export const Result = () => {
+  const {
+    actions,
+    state: {
+      form: { personalDetails, relations, education },
+    },
+  }: any = useStateMachine({ setFormStage })
+  useEffect(() => {}, [])
+
+  const handleBackButton = () => {
+    actions.setFormStage('relations')
+  }
+
+  return (
+    <FormStageWrapper
+      backButton={
+        <Button
+          variant={'contained'}
+          color={'secondary'}
+          onClick={handleBackButton}
+        >
+          tagasi
+        </Button>
+      }
+    >
+      <Box padding={'16px'}>
+        <Typography variant="h4">Tulemus</Typography>
+        <Typography variant="body2">Isikuandmed</Typography>
+        <pre>{JSON.stringify(personalDetails, null, 2)}</pre>
+        <Typography variant="body2">Lähisuhted</Typography>
+        <pre>{JSON.stringify(relations, null, 2)}</pre>
+        <Typography variant="body2">Haridus</Typography>
+        <pre>{JSON.stringify(education, null, 2)}</pre>
+      </Box>
+    </FormStageWrapper>
+  )
+}
