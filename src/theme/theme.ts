d07fb7412b97a3b02c95da9cf0b5ddb4b621c695
rkaasi <rkaasi@ttu.ee>
2023-10-16 09:28:24 +0300

    change-theme-Config

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,32 +1,42 @@
-import createTheme from "@mui/material/styles/createTheme";
+import createTheme from '@mui/material/styles/createTheme'
 
 export const theme = createTheme({
-	components: {
-		MuiGrid: {
-			styleOverrides: {
-				root: {
-					boxSizing: "border-box",
-				},
-			},
-		},
-	},
-	typography: {
-		body1: {
-			fontSize: "15px",
-		},
-	},
-	palette: {
-		secondary: {
-			main: "#fbd020",
-		},
-	},
-	breakpoints: {
-		values: {
-			xs: 0,
-			sm: 720,
-			md: 1024,
-			lg: 1200,
-			xl: 1536,
-		},
-	},
-});
+  components: {
+    MuiGrid: {
+      styleOverrides: {
+        root: {
+          boxSizing: 'border-box',
+        },
+      },
+    },
+  },
+  palette: {
+    secondary: {
+      main: '#fbd020',
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 720,
+      md: 1024,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+  typography: {
+    body1: {
+      fontSize: '15px',
+    },
+    body2: {
+      fontSize: '1rem',
+      fontWeight: 600,
+      marginTop: '24px',
+    },
+    subtitle2: {
+      fontWeight: 600,
+      fontSize: '13px',
+      color: '#888',
+    },
+  },
+})
