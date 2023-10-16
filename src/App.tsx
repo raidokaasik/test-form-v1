a3b69c8ec5f5340de8416928a2fd23ae012aacb5
rkaasi <rkaasi@ttu.ee>
2023-10-16 09:22:54 +0300

    add-necessary-date-picker-and-my-little-state-machine-and-lodash-packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { AppLayout } from "./layout/AppLayout";
-import { NavMenu } from "./components/NavMenu";
-import { Hiring } from "./pages/Hiring";
-import { FormContextProvider } from "./context/formContext";
-import { Routes, Route } from "react-router-dom";
+import { AppLayout } from './layout/AppLayout'
+import { NavMenu } from './components/NavMenu'
+import { Hiring } from './pages/Hiring'
 
 export const App = () => {
-	return (
-		<FormContextProvider>
-			<AppLayout>
-				<NavMenu />
-				<Hiring />
-				{/* <Routes>
-					<Route path="/" />
-					<Route path="/kandideeri" element={<Hiring />} />
-				</Routes> */}
-			</AppLayout>
-		</FormContextProvider>
-	);
-};
+  return (
+    <AppLayout>
+      <NavMenu />
+      <Hiring />
+    </AppLayout>
+  )
+}
