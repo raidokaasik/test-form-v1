1af1e6dc1f9644b42f6bb6032b86cda5f3b54c4b
rkaasi <rkaasi@ttu.ee>
2023-10-16 18:04:45 +0300

    add-previous-experience-stage-and-validation

---
+++ src/features/form/formStages/PreviousExperience.tsx
@@ -0,0 +1,97 @@
+import Typography from '@mui/material/Typography'
+import { useForm, FormProvider } from 'react-hook-form'
+import { useStateMachine } from 'little-state-machine'
+import { setFormStage, setPreviousExperience } from '../formActions/actions'
+import { FormStageWrapper } from '../formWrappers/FormStageWrapper'
+import { NextButton } from 'src/components/NextButton'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import {
+  IWorkExperience,
+  WorkExperience,
+  workExperienceDefaultValue,
+} from './previousExperience/WorkExperience'
+import {
+  ArmyExperience,
+  IArmyExperience,
+  armyExperienceDefaultValue,
+} from './previousExperience/ArmyExperience'
+
+export interface IPerviousExperience {
+  workExperience: IWorkExperience[] | null
+  hasArmyExperience: boolean
+  armyExperience: IArmyExperience[] | null
+}
+
+export const PerviousExperienceStage = () => {
+  const {
+    state: {
+      form: { previousExperience },
+    },
+    actions,
+  } = useStateMachine({
+    setFormStage,
+    setPreviousExperience,
+  })
+
+  const methods = useForm<IPerviousExperience>({
+    defaultValues: {
+      workExperience: previousExperience?.workExperience ?? [
+        workExperienceDefaultValue,
+      ],
+      hasArmyExperience: previousExperience?.hasArmyExperience ?? false,
+      armyExperience: previousExperience?.armyExperience ?? [
+        armyExperienceDefaultValue,
+      ],
+    },
+    mode: 'onBlur',
+  })
+
+  const onSubmit = (data: IPerviousExperience) => {
+    console.log('DATA::', data)
+    actions.setPreviousExperience(data)
+    actions.setFormStage('result')
+  }
+
+  const handleBackButton = () => {
+    const currentValues = methods.getValues()
+    actions.setPreviousExperience(currentValues)
+    actions.setFormStage('education')
+  }
+
+  return (
+    <FormStageWrapper
+      nextButton={<NextButton id="previousExperienceData" label="Järgmine" />}
+      backButton={
+        <Button
+          variant={'contained'}
+          color={'secondary'}
+          onClick={handleBackButton}
+        >
+          tagasi
+        </Button>
+      }
+    >
+      <Box pl={'16px'} pr={'16px'} pt={'16px'}>
+        <Typography variant="h5" fontWeight={600}>
+          VARASEM ELUKÄIK JA TEGEVUS
+        </Typography>
+      </Box>
+      <FormProvider {...methods}>
+        <form
+          id="previousExperienceData"
+          noValidate
+          onSubmit={methods.handleSubmit(onSubmit)}
+          style={{
+            width: '100%',
+            boxSizing: 'border-box',
+            padding: '16px',
+          }}
+        >
+          <WorkExperience />
+          <ArmyExperience />
+        </form>
+      </FormProvider>
+    </FormStageWrapper>
+  )
+}
