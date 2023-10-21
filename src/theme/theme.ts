5d6c4946a29944fd753ecb892a8798aa35beaafe
rkaasi <rkaasi@ttu.ee>
2023-10-21 18:04:30 +0300

    add-custom-font-mulish

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,7 +1,50 @@
 import createTheme from '@mui/material/styles/createTheme'
 
+const SECONDARY_MAIN = '#0072CE'
+
 export const theme = createTheme({
   components: {
+    MuiToggleButtonGroup: {
+      styleOverrides: {
+        root: {
+          '.MuiToggleButtonGroup-grouped:not(:first-of-type)': {
+            borderLeft: 'unset',
+            marginLeft: 0,
+          },
+        },
+      },
+    },
+    MuiToggleButton: {
+      styleOverrides: {
+        root: {
+          boxSizing: 'border-box',
+          fontSize: '12px',
+          fontWeight: '700',
+          borderRadius: '15px',
+          height: '24px',
+          width: '46px',
+          color: SECONDARY_MAIN,
+          border: `2px solid ${SECONDARY_MAIN}`,
+          '&.Mui-selected': {
+            background: SECONDARY_MAIN,
+            color: '#fff',
+            '&:hover': {
+              background: SECONDARY_MAIN,
+            },
+          },
+          '&:hover': {
+            background: '#edf7ff',
+          },
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          fontWeight: '700',
+        },
+      },
+    },
     MuiGrid: {
       styleOverrides: {
         root: {
@@ -12,7 +55,7 @@ export const theme = createTheme({
   },
   palette: {
     secondary: {
-      main: '#fbd020',
+      main: SECONDARY_MAIN,
     },
   },
   breakpoints: {
@@ -25,6 +68,7 @@ export const theme = createTheme({
     },
   },
   typography: {
+    fontFamily: ['Mulish Variable', 'Roboto', 'sans-serif'].join(','),
     body1: {
       fontSize: '15px',
     },
