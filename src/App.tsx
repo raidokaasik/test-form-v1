b9f090bf42e66dcd36dd750b1676bb95ff608ca1
rkaasi <rkaasi@ttu.ee>
2023-10-21 18:04:30 +0300

    add-custom-font-mulish

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { AppLayout } from './layout/AppLayout'
 import { NavMenu } from './components/NavMenu'
 import { Hiring } from './pages/Hiring'
+import '@fontsource-variable/mulish'
 
 export const App = () => {
   return (
